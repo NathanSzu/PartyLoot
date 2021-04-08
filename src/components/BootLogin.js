2a723fd2c4ef9de472dc26317162a999b2387ecd
Nathan Szurek <nathan.szurek@yahoo.com>
2021-04-08 15:52:10 -0500

    Added basic regex to login/signup and scaffolded validation triggers for future use

--- src/components/BootLogin.js
+++ src/components/BootLogin.js
@@ -5,16 +5,40 @@ import { Form, Button} from 'react-bootstrap'
 import app from '../utils/firebase'
 
 export default function BootLogin({ login, setLogin }) {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [checkPassword, setCheckPassword] = useState(null)
+    const [emailValid, setEmailValid] = useState(null)
+    const [passwordMatch, setPasswordMatch] = useState(null)
 
     const toggleLogin = () => {
         if (login) { setLogin(false) }
         else { setLogin(true) }
     }
-    var email;
-    var password;
 
     const test = () => {
-        console.log(email)
+        console.log('email: ', email)
+        console.log('password: ', password)
+        console.log('checkPassword: ', checkPassword)
+        console.log('validEmail: ', emailValid)
+        console.log('validPassword: ', passwordMatch)
+    }
+
+    const validateEmail = (e) => {
+        const emailRequirements = /[a-z0-9]+@+[a-z0-9]+[.]+[a-z0-9]/;
+        if (!emailRequirements.test(e.target.value)) {
+            setEmailValid(false)
+        } else {
+            setEmailValid(true)
+        }
+    }
+
+    const validatePassword = (e) => {
+        if (password === checkPassword) {
+            setPasswordMatch(true)
+        } else {
+            setPasswordMatch(false)
+        }
     }
 
     const signUp = () => {
@@ -48,7 +72,7 @@ export default function BootLogin({ login, setLogin }) {
         <Form>
             <Form.Group controlId="Email">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="Enter email" onChange={(e) => {setEmail(e.target.value);  validateEmail(e)}} />
                 <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
             </Form.Text>
@@ -56,13 +80,13 @@ export default function BootLogin({ login, setLogin }) {
 
             <Form.Group controlId="Password">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Control type="password" placeholder="Password" onChange={(e) => {setPassword(e.target.value)}} />
             </Form.Group>
 
             { !login ?
                 < Form.Group controlId="PasswordConfirm">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control type="password" placeholder="Confirm Password" onChange={(e) => {setCheckPassword(e.target.value); validatePassword()}} />
                 </Form.Group>
                 :
                 null
@@ -77,11 +101,11 @@ export default function BootLogin({ login, setLogin }) {
 
             {
                 login ?
-                    <Button variant="dark" type="submit">
+                    <Button variant="dark" type="submit" onClick={(e) => {e.preventDefault(); test()}} >
                         Login
                     </Button>
                     :
-                    <Button variant="dark" type="submit">
+                    <Button variant="dark" type="submit" onClick={(e) => {e.preventDefault(); test()}} >
                         Create Account
                     </Button>
             }
