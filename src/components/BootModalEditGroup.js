857529b1978a306f72904373bd28d36762e87ef3
Nathan Szurek <nathan.szurek@yahoo.com>
2021-04-20 10:24:22 -0500

    Created a group edit modal

---
+++ src/components/BootModalEditGroup.js
@@ -0,0 +1,60 @@
+import React, { useState, useContext, useRef } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+import fb from 'firebase';
+import firebase from '../utils/firebase';
+import { AuthContext } from '../utils/AuthContext';
+import gear from '../assets/gear-fill.svg'
+
+export default function BootModalEditGroup() {
+    const { currentUser } = useContext(AuthContext)
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const nameRef = useRef()
+
+    const db = firebase.firestore();
+
+    const editGroup = () => {
+        console.log(nameRef.current.value)
+        db.collection('groups').add({
+            groupName: nameRef.current.value,
+            members: [currentUser.uid],
+            created: fb.firestore.FieldValue.serverTimestamp()
+        })
+        .then((docRef) => {
+            console.log("Group added with ID: ", docRef.id);
+            handleClose();
+        })
+        .catch((error) => {
+            console.error("Error creating new group: ", error);
+            handleClose();
+        });
+    }
+
+    return (
+        <>
+            <Button variant='dark' className='p-1' onClick={handleShow}><img src={gear} fill='white'></img></Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Form>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Edit your group.</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <Form.Group controlId='groupName'>
+                            <Form.Control ref={nameRef} type='text' placeholder='Group name' />
+                        </Form.Group>
+                    </Modal.Body>
+
+                    <Modal.Footer>
+                        <Button variant='dark' type='submit' onClick={(e) => {e.preventDefault(); editGroup()}}>
+                            Save
+                        </Button>
+                    </Modal.Footer>
+                </Form>
+            </Modal>
+        </>
+    )
+}
