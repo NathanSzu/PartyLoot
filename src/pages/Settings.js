62290c7a52b4c91fb3789cf8eeaa244f7b7ba099
Nathan Szurek <nathan.szurek@yahoo.com>
2021-04-25 11:40:36 -0500

    Started work on framework to assign friend codes

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,12 +1,37 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../utils/AuthContext';
+import firebase from '../utils/firebase';
 
 export default function Settings() {
-    const { currentUser } = useContext(AuthContext)
+    const { currentUser } = useContext(AuthContext);
+    const db = firebase.firestore();
+
+    useEffect(() => {
+        db.collection('users').doc(`${currentUser.uid}`)
+            .onSnapshot((doc) => {
+                console.log('Current data: ', doc.data());
+            });
+    }, [])
+
+    const initiateUsername = () => {
+
+    }
+
+    const initiateGroupCode = () => {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+        let code = '';
+        for (let i = 0; i < 2; i++) {
+            code += alphabet[Math.floor(Math.random() * alphabet.length)];
+            code += Math.floor(Math.random() * 10);
+        }
+        db.collection('users').doc(`${currentUser.uid}`).set({code: code})
+        .then(() => {console.log('Code created!')})
+        .catch((error) => {console.error('Error creating code: ', error)});
+    }
 
     return (
         <div>
-            
+            <button>test</button>
         </div>
     )
 }
