c2d6ee3bbb277393d275b0f7759e2aea284bb48d
Nathan Szurek <nathan.szurek@yahoo.com>
2021-04-27 18:16:27 -0500

    Added code to randomly generate a user code if one does not exist

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -5,15 +5,36 @@ import firebase from '../utils/firebase';
 export default function Settings() {
     const { currentUser } = useContext(AuthContext);
     const db = firebase.firestore();
+    const [groupCode, setGroupCode] = useState();
 
     useEffect(() => {
+        if (currentUser.displayName) { console.log(currentUser.displayName) }
+        else { }
+        db.collection('random-data').doc('identification')
+            .onSnapshot((doc) => {
+                initiateUsername(doc.data().usernames)
+                console.log('Current data: ', doc.data().usernames);
+            });
+
         db.collection('users').doc(`${currentUser.uid}`)
             .onSnapshot((doc) => {
+                if (doc.data().code) { console.log(doc.data().code) }
+                else { initiateGroupCode() }
                 console.log('Current data: ', doc.data());
+                setGroupCode(doc.data());
             });
-    }, [])
+    }, [currentUser])
 
-    const initiateUsername = () => {
+    const initiateUsername = (arr) => {
+        firebase.auth().currentUser.updateProfile({
+            displayName: arr[Math.floor(Math.random() * arr.length)]
+        }).then(function () {
+            // Update successful.
+            console.log('profile updated')
+        }).catch(function (error) {
+            // An error happened.
+            console.log(error)
+        });
 
     }
 
@@ -24,9 +45,9 @@ export default function Settings() {
             code += alphabet[Math.floor(Math.random() * alphabet.length)];
             code += Math.floor(Math.random() * 10);
         }
-        db.collection('users').doc(`${currentUser.uid}`).set({code: code})
-        .then(() => {console.log('Code created!')})
-        .catch((error) => {console.error('Error creating code: ', error)});
+        db.collection('users').doc(`${currentUser.uid}`).set({ code: code }, { merge: true })
+            .then(() => { console.log('Code created!') })
+            .catch((error) => { console.error('Error creating code: ', error) });
     }
 
     return (
