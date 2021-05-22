4caa763186ddb42cde33c0644cab8392d761705e
Nathan Szurek <nathan.szurek@yahoo.com>
2021-05-22 17:29:03 -0500

    Created a more descriptive home page

--- src/components/BootJumbo.js
+++ src/components/BootJumbo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Jumbotron, Container, Button } from 'react-bootstrap';
+import { Jumbotron, Container, Button, Row, Col } from 'react-bootstrap';
 
 export default function BootJumbo({ setWelcome, setLogin }) {
     const showForms = () => {
@@ -10,12 +10,25 @@ export default function BootJumbo({ setWelcome, setLogin }) {
     return (
         <Jumbotron fluid>
             <Container>
-                <h1>Fluid jumbotron</h1>
-                <p>
-                    This is a modified jumbotron that occupies the entire horizontal space of
-                    its parent.
-                </p>
-                <Button variant='dark' onClick={showForms} >Login</Button>
+                <Row border='dark'>
+                    <Col lg={9} className='mr-auto ml-auto'>
+                        <h1 className='text-center mb-4'>Welcome to Party Loot</h1>
+                        <p className='text-center'>This app allows users to create shared inventories to manage loot and items for any roleplaying party.</p>
+                        <p className='text-center'>Tap <strong>Get Started</strong> to set up a free account or <strong>Patreon</strong> if you'd like to support this free application.</p>
+
+                        <h2 className='text-center mt-5 mb-4'>This is a Progressive Web Application</h2>
+                        <p className='text-center mb-4'>With this page open in your mobile browser select <strong>add to home screen</strong> for the full experience.</p>
+                    </Col>
+                </Row>
+                <Row className='justify-content-around mt-3'>
+                    <a href='https://www.patreon.com/appsbynathan' className='w-50' target='blank'>
+                        <Button className='w-100' variant='dark'>Patreon</Button>
+                    </a>
+                </Row>
+                <Row className='justify-content-around mt-3'>
+                    <Button className='w-50' variant='dark' onClick={showForms} >Get Started</Button>
+                </Row>
+
             </Container>
         </Jumbotron>
     )
