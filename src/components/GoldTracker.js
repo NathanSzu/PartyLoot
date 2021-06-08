686ec5d3e88334476d40524e4ed514cee5ed70ee
Nathan Szurek <nathan.szurek@yahoo.com>
2021-06-08 18:01:55 -0500

    Created functions to allow a working currency tracker

--- src/components/GoldTracker.js
+++ src/components/GoldTracker.js
@@ -1,38 +1,75 @@
-import React from 'react'
+import React, { useRef, useContext } from 'react'
 import { Row, Card, Col, Form } from 'react-bootstrap';
-import gold from '../assets/currency_gold.svg';
-import silver from '../assets/currency_silver.svg';
-import copper from '../assets/currency_copper.svg';
+import goldImg from '../assets/currency_gold.svg';
+import silverImg from '../assets/currency_silver.svg';
+import copperImg from '../assets/currency_copper.svg';
+import { AuthContext } from '../utils/AuthContext';
+import { GroupContext } from '../utils/GroupContext';
+import firebase from '../utils/firebase';
 
-export default function GoldTracker() {
+export default function GoldTracker({ gold, silver, copper, misc1, currencyRef }) {
+    const { currentUser } = useContext(AuthContext);
+    const { currentGroup } = useContext(GroupContext);
+
+    const goldRef = useRef();
+    const silverRef = useRef();
+    const copperRef = useRef();
+    const misc1Ref = useRef();
+
+    const updateGold = () => {
+        currencyRef.doc('gold').set({
+            name: 'gold',
+            qty: goldRef.current.value
+        })
+    };
+    const updateSilver = () => {
+        currencyRef.doc('silver').set({
+            name: 'silver',
+            qty: silverRef.current.value
+        })
+    };
+    const updateCopper = () => {
+        currencyRef.doc('copper').set({
+            name: 'copper',
+            qty: copperRef.current.value
+        })
+    };
+    const updateMisc1 = () => {
+        currencyRef.doc('misc1').set({
+            name: 'misc1',
+            qty: misc1Ref.current.value
+        })
+    };
+
+
     return (
         <>
             <Row>
                 <Col xs={1} className='p-0'>
-                    <Form.Label className='text-right'><img src={gold} className='w-100'></img></Form.Label>
+                    <Form.Label className='text-right'><img src={goldImg} className='w-100'></img></Form.Label>
                 </Col>
                 <Col xs={5} className='pl-2'>
-                    <Form.Control type='number' />
+                    <Form.Control type='number' defaultValue={gold && gold[0] && gold[0].qty ? gold[0].qty : null} ref={goldRef} onChange={updateGold} />
                 </Col>
                 <Col xs={1} className='p-0'>
-                    <Form.Label className='text-right'><img src={silver} className='w-100'></img></Form.Label>
+                    <Form.Label className='text-right'><img src={silverImg} className='w-100'></img></Form.Label>
                 </Col>
                 <Col xs={5} className='pl-2'>
-                    <Form.Control type='number' />
+                    <Form.Control type='number' defaultValue={silver && silver[0] && silver[0].qty ? silver[0].qty : null} ref={silverRef} onChange={updateSilver} />
                 </Col>
             </Row>
             <Row className='mt-2'>
                 <Col xs={1} className='p-0'>
-                    <Form.Label className='text-right'><img src={copper} className='w-100'></img></Form.Label>
+                    <Form.Label className='text-right'><img src={copperImg} className='w-100'></img></Form.Label>
                 </Col>
                 <Col xs={5} className='pl-2'>
-                    <Form.Control type='number' />
+                    <Form.Control type='number' defaultValue={copper && copper[0] && copper[0].qty ? copper[0].qty : null} ref={copperRef} onChange={updateCopper} />
                 </Col>
                 <Col xs={1} className='p-0'>
-                    <Form.Label className='text-right'><img src={copper} className='w-100'></img></Form.Label>
+                    <Form.Label className='text-right'><img src={copperImg} className='w-100'></img></Form.Label>
                 </Col>
                 <Col xs={5} className='pl-2'>
-                    <Form.Control type='number' />
+                    <Form.Control type='number' defaultValue={misc1 && misc1[0] && misc1[0].qty ? misc1[0].qty : null} ref={misc1Ref} onChange={updateMisc1} />
                 </Col>
             </Row>
         </>
--- src/pages/Loot.js
+++ src/pages/Loot.js
@@ -17,18 +17,43 @@ export default function Loot() {
 
   const db = firebase.firestore();
   const lootRef = db.collection('groups').doc(currentGroup).collection('loot');
-  const query = lootRef.orderBy('created');
+  const currencyRef = db.collection('groups').doc(currentGroup).collection('currency');
+
+  const query = lootRef.orderBy('created', 'desc');
+  const goldQuery = currencyRef.where('name', '==', 'gold');
+  const silverQuery = currencyRef.where('name', '===', 'silver');
+  const copperQuery = currencyRef.where('name', '===', 'copper');
+  const misc1Query = currencyRef.where('name', '===', 'misc1');
+
   const [lootItems, loading, error] = useCollectionData(query, { idField: 'id' });
+  const [gold] = useCollectionData(goldQuery, { idField: 'id' });
+  const [silver] = useCollectionData(silverQuery, { idField: 'id' });
+  const [copper] = useCollectionData(copperQuery, { idField: 'id' });
+  const [misc1] = useCollectionData(misc1Query, { idField: 'id' });
 
   useEffect(() => {
     error && console.log('Error loading items: ', error)
   }, [lootItems])
 
+  // useEffect(() => {
+  //   gold && console.log('gold', gold[0])
+  // }, [gold])
+
+  // useEffect(() => {
+  //   silver && console.log('silver', silver[0])
+  // }, [silver])
+
+  // useEffect(() => {
+  //   copper && console.log('copper', copper[0])
+  // }, [copper])
+
+
+
   return (
     <>
     <Card className='mt-2'>
       <Card.Header>
-        <GoldTracker />
+        <GoldTracker gold={gold} silver={silver} copper={copper} misc1={misc1} currencyRef={currencyRef} />
       </Card.Header>
     </Card>
     <Card className='mt-2 mb-2'>
