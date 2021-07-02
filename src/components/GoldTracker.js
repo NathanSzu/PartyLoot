42452216a07c00996bc36682d52b5be576ac662f
Nathan Szurek <nathan.szurek@yahoo.com>
2021-07-02 12:53:04 -0500

    Implemented color picker for all currency types

--- src/components/GoldTracker.js
+++ src/components/GoldTracker.js
@@ -36,6 +36,11 @@ export default function GoldTracker() {
     const currency6Ref = useRef();
 
     const color1Ref = useRef();
+    const color2Ref = useRef();
+    const color3Ref = useRef();
+    const color4Ref = useRef();
+    const color5Ref = useRef();
+    const color6Ref = useRef();
 
     const updateCurrency = (currency, currencyValueRef) => {
         currencyRef.doc(currency).update({
@@ -63,46 +68,45 @@ export default function GoldTracker() {
                     </Accordion.Toggle>
                     <Accordion.Collapse eventKey='0'>
                         <Card.Body>
-                            <Row className='pl-2'>
+                            <Row className='pl-1'>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Control type='color' defaultValue={'#ffbb00'} value={currency1 && currency1[0] && currency1[0].color || '#ffbb00'} ref={color1Ref} className='p-0 border-0' onChange={() => { updateColor('currency1', color1Ref) }} />
-                                    {/* <Form.Label className='text-right'><img alt='Currency1' src={goldImg} className='w-100'></img></Form.Label> */}
+                                    <Form.Control type='color' value={currency1 && currency1[0] && currency1[0].color || '#ffbb00'} ref={color1Ref} className='p-0 border-0' onChange={() => { updateColor('currency1', color1Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency1 && currency1[0] && currency1[0].qty ? currency1[0].qty : null} ref={currency1Ref} onChange={() => { updateCurrency('currency1', currency1Ref) }} />
                                 </Col>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Label className='text-right'><img alt='Currency2' src={silverImg} className='w-100'></img></Form.Label>
+                                    <Form.Control type='color' value={currency2 && currency2[0] && currency2[0].color || '#bdbdbd'} ref={color2Ref} className='p-0 border-0' onChange={() => { updateColor('currency2', color2Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency2 && currency2[0] && currency2[0].qty ? currency2[0].qty : null} ref={currency2Ref} onChange={() => { updateCurrency('currency2', currency2Ref) }} />
                                 </Col>
                             </Row>
-                            <Row className='mt-2 pl-2'>
+                            <Row className='mt-2 pl-1'>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Label className='text-right'><img alt='Currency3' src={copperImg} className='w-100'></img></Form.Label>
+                                    <Form.Control type='color' value={currency3 && currency3[0] && currency3[0].color || '#d27e1e'} ref={color3Ref} className='p-0 border-0' onChange={() => { updateColor('currency3', color3Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency3 && currency3[0] && currency3[0].qty ? currency3[0].qty : null} ref={currency3Ref} onChange={() => { updateCurrency('currency3', currency3Ref) }} />
                                 </Col>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Label className='text-right'><img alt='Currency4' src={copperImg} className='w-100'></img></Form.Label>
+                                    <Form.Control type='color' value={currency4 && currency4[0] && currency4[0].color || '#ffffff'} ref={color4Ref} className='p-0 border-0' onChange={() => { updateColor('currency4', color4Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency4 && currency4[0] && currency4[0].qty ? currency4[0].qty : null} ref={currency4Ref} onChange={() => { updateCurrency('currency4', currency4Ref) }} />
                                 </Col>
                             </Row>
-                            <Row className='mt-2 pl-2'>
+                            <Row className='mt-2 pl-1'>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Label className='text-right'><img alt='Currency5' src={copperImg} className='w-100'></img></Form.Label>
+                                    <Form.Control type='color' value={currency5 && currency5[0] && currency5[0].color || '#ffffff'} ref={color5Ref} className='p-0 border-0' onChange={() => { updateColor('currency5', color5Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency5 && currency5[0] && currency5[0].qty ? currency5[0].qty : null} ref={currency5Ref} onChange={() => { updateCurrency('currency5', currency5Ref) }} />
                                 </Col>
                                 <Col xs={1} className='p-0'>
-                                    <Form.Label className='text-right'><img alt='Currency6' src={copperImg} className='w-100'></img></Form.Label>
+                                    <Form.Control type='color' value={currency6 && currency6[0] && currency6[0].color || '#ffffff'} ref={color6Ref} className='p-0 border-0' onChange={() => { updateColor('currency6', color6Ref) }} />
                                 </Col>
-                                <Col xs={5} className='pl-2'>
+                                <Col xs={5} className='pl-1 pr-1'>
                                     <Form.Control type='number' defaultValue={currency6 && currency6[0] && currency6[0].qty ? currency6[0].qty : null} ref={currency6Ref} onChange={() => { updateCurrency('currency6', currency6Ref) }} />
                                 </Col>
                             </Row>
