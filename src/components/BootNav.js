2c1cf9d0a2442e6f9340442794cca599e6faf094
Nathan Szurek <nathanszurek@Nathans-MacBook-Pro.local>
2021-10-04 20:16:40 -0500

    Routing tweaks

--- src/components/BootNav.js
+++ src/components/BootNav.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import firebase from '../utils/firebase';
 import { AuthContext } from '../utils/AuthContext';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function BootNav() {
 	const location = useLocation();
@@ -21,26 +22,32 @@ export default function BootNav() {
 	};
 
 	return (
-		<Navbar bg="light" expand="false">
-			<Navbar.Brand href="/groups" className="fancy-font">
-				Party Loot
-			</Navbar.Brand>
+		<Navbar bg="light" expand="false" collapseOnSelect>
+			<LinkContainer to='/groups'>
+				<Navbar.Brand className="fancy-font">
+					Party Loot
+				</Navbar.Brand>
+			</LinkContainer>
 			{location.pathname === '/' ? null : 
             <>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto">
                         {currentUser.uid === ' ' ? null : (
-                            <Nav.Link href="/" onClick={logOut}>
-                                Sign Out
-                            </Nav.Link>
+                            <Nav.Link href="/" onClick={logOut}>Sign Out</Nav.Link>
                         )}
                         {location.pathname === '/' ||
                         location.pathname === '/groups' ||
                         location.pathname === '/forgot-password' ? null : (
-                            <Nav.Link href="/groups">Groups</Nav.Link>
+							<LinkContainer to='/groups'>
+                            	<Nav.Link>Groups</Nav.Link>
+							</LinkContainer>
                         )}
-                        {currentUser.uid === ' ' ? null : <Nav.Link href="/user-settings">Settings</Nav.Link>}
+                        {currentUser.uid === ' ' || location.pathname === '/user-settings' ? null : 
+						<LinkContainer to='/user-settings'>
+							<Nav.Link>Settings</Nav.Link>
+						</LinkContainer>
+						}
                     </Nav>
                 </Navbar.Collapse>
             </>}
