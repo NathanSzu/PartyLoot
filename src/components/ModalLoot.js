7425ae88f12a01ab110bc34c99b2b709a6bb9e84
Nathan Szurek <nathanszurek@Nathans-MacBook-Pro.local>
2021-10-06 16:24:21 -0500

    Client feedback alert added to item modal

--- src/components/ModalLoot.js
+++ src/components/ModalLoot.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useRef } from 'react';
-import { Modal, Button, Form, Row, Col } from 'react-bootstrap';
+import { Modal, Button, Form, Row, Col, Alert } from 'react-bootstrap';
 import edit from '../assets/pencil-square.svg';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
 import { GroupContext } from '../utils/GroupContext';
@@ -9,191 +9,279 @@ import DropdownAddItem from './DropdownAddItem';
 import SearchOpen5E from './SearchOpen5E';
 
 export default function ModalLoot({ item, idx }) {
-    const { currentGroup } = useContext(GroupContext);
+	const { currentGroup } = useContext(GroupContext);
 
-    const db = firebase.firestore();
-    const itemRef = db.collection('groups').doc(`${currentGroup}`).collection('loot').doc(`${item.id}`);
-    const groupRef = db.collection('groups').doc(currentGroup);
+	const db = firebase.firestore();
+	const itemRef = db.collection('groups').doc(`${currentGroup}`).collection('loot').doc(`${item.id}`);
+	const groupRef = db.collection('groups').doc(currentGroup);
 
-    const [show, setShow] = useState(false)
-    const [deleteConfirmation, setDeleteConfirmation] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [searchSRD, setSearchSRD] = useState(false);
-    const [SRDContent, setSRDContent] = useState({})
+	const [ show, setShow ] = useState(false);
+	const [ deleteConfirmation, setDeleteConfirmation ] = useState(false);
+	const [ loading, setLoading ] = useState(false);
+	const [ searchSRD, setSearchSRD ] = useState(false);
+	const [ SRDContent, setSRDContent ] = useState({});
+	const [ itemValidations, setItemValidations ] = useState('');
 
-    const [partyData] = useDocumentData(groupRef);
+	const [ partyData ] = useDocumentData(groupRef);
 
-    const nameRef = useRef();
-    const descRef = useRef();
-    const chargeRef = useRef();
-    const chargesRef = useRef();
-    const tagsRef = useRef();
-    const ownerRef = useRef();
+	const nameRef = useRef();
+	const descRef = useRef();
+	const chargeRef = useRef();
+	const chargesRef = useRef();
+	const tagsRef = useRef();
+	const ownerRef = useRef();
 
-    const handleClose = () => {setShow(false); setSRDContent({}); setSearchSRD(false);}
-    const handleShow = () => setShow(true);
+	const handleClose = () => {
+        setItemValidations('')
+		setShow(false);
+		setSRDContent({});
+		setSearchSRD(false);
+	};
+	const handleShow = () => setShow(true);
 
-    const addLoot = () => {
-        if (!nameRef.current.value || !descRef.current.value) { return }
-        if (ownerRef.current.value === 'Select Owner') { ownerRef.current.value = '' }
-        setLoading(true);
-        db.collection('groups').doc(`${currentGroup}`).collection('loot').add({
-            itemName: nameRef.current.value,
-            itemDesc: descRef.current.value,
-            currCharges: chargeRef.current.value,
-            maxCharges: chargesRef.current.value,
-            itemTags: tagsRef.current.value,
-            owner: ownerRef.current.value,
-            created: fb.firestore.FieldValue.serverTimestamp()
-        })
-            .then((docRef) => {
-                setLoading(false);
-                handleClose();
-            })
-            .catch((error) => {
-                console.error("Error creating new group: ", error);
-                setLoading(false);
-                handleClose();
-            });
-    }
+	const addLoot = () => {
+		if (!nameRef.current.value || !descRef.current.value) {
+			setItemValidations('name and description')
+            return;
+		}
+		if (ownerRef.current.value === 'Select Owner') {
+			ownerRef.current.value = '';
+		}
+		setLoading(true);
+		db
+			.collection('groups')
+			.doc(`${currentGroup}`)
+			.collection('loot')
+			.add({
+				itemName: nameRef.current.value,
+				itemDesc: descRef.current.value,
+				currCharges: chargeRef.current.value,
+				maxCharges: chargesRef.current.value,
+				itemTags: tagsRef.current.value,
+				owner: ownerRef.current.value,
+				created: fb.firestore.FieldValue.serverTimestamp()
+			})
+			.then((docRef) => {
+				setLoading(false);
+				handleClose();
+			})
+			.catch((error) => {
+				console.error('Error creating new group: ', error);
+				setLoading(false);
+				handleClose();
+			});
+	};
 
-    const editLoot = () => {
-        // Does not call update function if the group name or description is left empty.
-        if (!nameRef.current.value || !descRef.current.value) {
-            handleClose();
-            return
-        }
-        if (ownerRef.current.value === 'Select Owner') { ownerRef.current.value = '' }
-        setLoading(true)
-        itemRef.update({
-            itemName: nameRef.current.value,
-            itemDesc: descRef.current.value,
-            currCharges: chargeRef.current.value,
-            maxCharges: chargesRef.current.value,
-            itemTags: tagsRef.current.value,
-            owner: ownerRef.current.value
-        })
-            .then(() => {
-                handleClose();
-                setLoading(false);
-            })
-            .catch((error) => {
-                // The document probably doesn't exist.
-                console.error('Error updating item: ', error);
-                handleClose();
-                setLoading(false);
-            });
-    }
+	const editLoot = () => {
+		// Does not call update function if the group name or description is left empty.
+		if (!nameRef.current.value || !descRef.current.value) {
+            setItemValidations('name and description')
+			return;
+		}
+		if (ownerRef.current.value === 'Select Owner') {
+			ownerRef.current.value = '';
+		}
+		setLoading(true);
+		itemRef
+			.update({
+				itemName: nameRef.current.value,
+				itemDesc: descRef.current.value,
+				currCharges: chargeRef.current.value,
+				maxCharges: chargesRef.current.value,
+				itemTags: tagsRef.current.value,
+				owner: ownerRef.current.value
+			})
+			.then(() => {
+				handleClose();
+				setLoading(false);
+			})
+			.catch((error) => {
+				// The document probably doesn't exist.
+				console.error('Error updating item: ', error);
+				handleClose();
+				setLoading(false);
+			});
+	};
 
-    const deleteItem = () => {
-        setLoading(true);
-        handleClose();
-        itemRef.delete()
-            .then(() => {
-                setLoading(false);
-            }).catch((error) => {
-                setLoading(false);
-                console.error('Error removing item: ', error);
-            });
-    }
+	const deleteItem = () => {
+		setLoading(true);
+		handleClose();
+		itemRef
+			.delete()
+			.then(() => {
+				setLoading(false);
+			})
+			.catch((error) => {
+				setLoading(false);
+				console.error('Error removing item: ', error);
+			});
+	};
 
-    return (
-        <>
-            {item ? <Button variant='dark' className='p-1 m-0' onClick={handleShow}><img alt='Edit Item' src={edit}></img></Button>
-                :
-                <Button variant='dark' onClick={handleShow} className='w-100 m-0 mr-auto ml-auto'>Add Item</Button>}
+	return (
+		<div>
+			{item ? (
+				<Button variant="dark" className="p-1 m-0" onClick={handleShow}>
+					<img alt="Edit Item" src={edit} />
+				</Button>
+			) : (
+				<Button variant="dark" onClick={handleShow} className="w-100 m-0 mr-auto ml-auto">
+					Add Item
+				</Button>
+			)}
 
+			<Modal show={show} onHide={handleClose}>
+				<Form className="texture-backer">
+					<Modal.Header closeButton>
+						{item ? (
+							<Modal.Title>{`Edit ${item.itemName}`}</Modal.Title>
+						) : (
+							<Modal.Title>Add an item!</Modal.Title>
+						)}
+					</Modal.Header>
 
-            <Modal show={show} onHide={handleClose}>
-                <Form className='texture-backer'>
-                    <Modal.Header closeButton>
-                        {item ? <Modal.Title>{`Edit ${item.itemName}`}</Modal.Title>
-                            :
-                            <Modal.Title>Add an item!</Modal.Title>}
-                    </Modal.Header>
+					{searchSRD ? (
+						<SearchOpen5E setSearchSRD={setSearchSRD} setSRDContent={setSRDContent} />
+					) : (
+						<div>
+							<Modal.Body>
+								<Row>
+									{!item && (
+										<Col xs={2} className="pr-0">
+											<DropdownAddItem setSearchSRD={setSearchSRD} />
+										</Col>
+									)}
+									<Col>
+										<Form.Group controlId="itemName">
+											<Form.Control
+												ref={nameRef}
+												defaultValue={(item && item.itemName) || SRDContent.name}
+												type="text"
+												placeholder="Item name"
+											/>
+										</Form.Group>
+									</Col>
+								</Row>
 
-                    {searchSRD ? <SearchOpen5E setSearchSRD={setSearchSRD} setSRDContent={setSRDContent} /> :
-                    <> 
-                        <Modal.Body>
-                            <Row>
-                                {!item &&
-                                <Col xs={2} className='pr-0'>
-                                    <DropdownAddItem setSearchSRD={setSearchSRD} />
-                                </Col>}
-                                <Col>
-                                    <Form.Group controlId='itemName'>
-                                        <Form.Control ref={nameRef} defaultValue={(item && item.itemName) || SRDContent.name} type='text' placeholder='Item name' />
-                                    </Form.Group>
-                                </Col>
-                            </Row>
+								<Row>
+									<Col xs={5}>
+										<Form.Group controlId="itemCharge">
+											<Form.Control
+												className="text-center"
+												ref={chargeRef}
+												defaultValue={item && item.currCharges}
+												type="number"
+												placeholder="Charge"
+											/>
+										</Form.Group>
+									</Col>
 
-                            <Row>
+									<Col xs={2} className="d-flex align-items-center justify-content-center">
+										/
+									</Col>
 
-                                <Col xs={5}>
-                                    <Form.Group controlId='itemCharge'>
-                                        <Form.Control className='text-center' ref={chargeRef} defaultValue={item && item.currCharges} type='number' placeholder='Charge' />
-                                    </Form.Group>
-                                </Col>
+									<Col xs={5}>
+										<Form.Group controlId="itemCharges">
+											<Form.Control
+												className="text-center"
+												ref={chargesRef}
+												defaultValue={item && item.maxCharges}
+												type="number"
+												placeholder="Charges"
+											/>
+										</Form.Group>
+									</Col>
+								</Row>
 
-                                <Col xs={2} className='d-flex align-items-center justify-content-center'>
-                                    /
-                                </Col>
+								<Form.Group controlId="itemDesc">
+									<Form.Control
+										ref={descRef}
+										as="textarea"
+										rows={4}
+										defaultValue={(item && item.itemDesc) || SRDContent.desc}
+										placeholder="Item description"
+									/>
+								</Form.Group>
 
-                                <Col xs={5}>
-                                    <Form.Group controlId='itemCharges'>
-                                        <Form.Control className='text-center' ref={chargesRef} defaultValue={item && item.maxCharges} type='number' placeholder='Charges' />
-                                    </Form.Group>
-                                </Col>
+								<Form.Group controlId="itemTags">
+									<Form.Control
+										ref={tagsRef}
+										type="text"
+										defaultValue={(item && item.itemTags) || SRDContent.type}
+										placeholder="Enter searchable item tags here"
+									/>
+								</Form.Group>
 
-                            </Row>
+								<Form.Group controlId="itemOwner">
+									<Form.Control as="select" defaultValue={item && item.owner} ref={ownerRef}>
+										<option>Select Owner</option>
+										{partyData &&
+											partyData.party &&
+											partyData.party.map((partyMember, idx) => (
+												<option key={idx}>{partyMember}</option>
+											))}
+									</Form.Control>
+								</Form.Group>
+                                {itemValidations && <Alert variant='warning'>Item {itemValidations} are required!</Alert>}
+							</Modal.Body>
 
+							<Modal.Footer className="justify-content-between">
+								{item ? (
+									<Button
+										as="input"
+										disabled={loading}
+										value="Save"
+										variant="dark"
+										type="submit"
+										onClick={(e) => {
+											e.preventDefault();
+											editLoot();
+										}}
+									/>
+								) : (
+									<Button
+										disabled={loading}
+										variant="dark"
+										type="submit"
+										onClick={(e) => {
+											e.preventDefault();
+											addLoot();
+										}}
+									>
+										Create
+									</Button>
+								)}
 
-                            <Form.Group controlId='itemDesc'>
-                                <Form.Control ref={descRef} as='textarea' rows={4} defaultValue={(item && item.itemDesc) || SRDContent.desc} placeholder='Item description' />
-                            </Form.Group>
+								{deleteConfirmation ? (
+									<Button
+										as="input"
+										value={`Yes, I'm sure. Delete!`}
+										disabled={loading}
+										variant="danger"
+										type="button"
+										onClick={(e) => {
+											e.preventDefault();
+											deleteItem();
+										}}
+									/>
+								) : null}
 
-                            <Form.Group controlId='itemTags'>
-                                <Form.Control ref={tagsRef} type='text' defaultValue={(item && item.itemTags) || SRDContent.type} placeholder='Enter searchable item tags here' />
-                            </Form.Group>
-
-                            <Form.Group controlId='itemOwner'>
-                                <Form.Control as='select' defaultValue={item && item.owner} ref={ownerRef}>
-                                    <option>Select Owner</option>
-                                    {partyData && partyData.party && partyData.party.map((partyMember, idx) => (
-                                        <option key={idx}>{partyMember}</option>
-                                    ))}
-                                </Form.Control>
-                            </Form.Group>
-
-                        </Modal.Body>
-
-
-                        <Modal.Footer className='justify-content-between'>
-
-                            {item ?
-                                <Button as='input' disabled={loading} value='Save' variant='dark' type='submit' onClick={(e) => { e.preventDefault(); editLoot() }} />
-                                :
-                                <Button disabled={loading} variant='dark' type='submit' onClick={(e) => { e.preventDefault(); addLoot() }}>
-                                    Create
-                                </Button>}
-
-                            {deleteConfirmation ?
-                                <Button as='input' value={`Yes, I'm sure. Delete!`} disabled={loading} variant='danger' type='button' onClick={(e) => { e.preventDefault(); deleteItem() }} />
-                                : null}
-
-                            {!deleteConfirmation && item ?
-                                <Button as='input' value='Delete' disabled={loading} variant='danger' type='button' onClick={(e) => { setDeleteConfirmation(true) }} />
-                                : null}
-
-
-
-                        </Modal.Footer>
-                    </>}
-                </Form>
-
-
-            </Modal>
-
-        </>
-    )
+								{!deleteConfirmation && item ? (
+									<Button
+										as="input"
+										value="Delete"
+										disabled={loading}
+										variant="danger"
+										type="button"
+										onClick={(e) => {
+											setDeleteConfirmation(true);
+										}}
+									/>
+								) : null}
+							</Modal.Footer>
+						</div>
+					)}
+				</Form>
+			</Modal>
+		</div>
+	);
 }
