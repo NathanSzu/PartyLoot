96781160be31ce0daaa62882fc0cea981bf7827f
Nathan Szurek <nathanszurek@Nathans-MacBook-Pro.local>
2021-10-08 15:33:12 -0500

    fix: fixed ui bug on large screens

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
     return (
         <div className='p-0'>
             { welcome ? <Jumbotron setLogin={setLogin} setWelcome={setWelcome} /> : null}
-            <Row className="justify-content-md-center">
+            <Row className="justify-content-md-center pl-3 pr-3">
                 <Col>
                     {!welcome ? <Login login={login} setLogin={setLogin} /> : null}
                 </Col>
