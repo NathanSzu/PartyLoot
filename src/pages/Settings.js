997b8d4f13dccbd9aa64834e53d83c3e1af6f25b
Nathan Szurek <nathan.szurek@yahoo.com>
2021-11-05 11:28:47 -0500

    ui: edited settings page styling and added support links

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,28 +1,28 @@
-import React, { useContext, useState } from 'react';
-import ModalEditUser from '../components/ModalEditUsername';
-import { Row, Col, Spinner, Alert, Button, Container } from 'react-bootstrap';
-import { AuthContext } from '../utils/contexts/AuthContext';
-import { useDocumentData } from 'react-firebase-hooks/firestore';
-import app from '../utils/firebase';
+import React, { useContext, useState } from "react";
+import ModalEditUser from "../components/ModalEditUsername";
+import { Row, Col, Spinner, Alert, Button, Container } from "react-bootstrap";
+import { AuthContext } from "../utils/contexts/AuthContext";
+import { useDocumentData } from "react-firebase-hooks/firestore";
+import app from "../utils/firebase";
 
 export default function Settings() {
 	const { currentUser, userRef } = useContext(AuthContext);
-	const [ loading, setLoading ] = useState(false);
-	const [ resetEmailSent, setResetEmailSent ] = useState(false);
+	const [loading, setLoading] = useState(false);
+	const [resetEmailSent, setResetEmailSent] = useState(false);
 
-	const [ userData ] = useDocumentData(userRef);
+	const [userData] = useDocumentData(userRef);
 
 	const passwordReset = (email) => {
 		setLoading(true);
 		app
 			.auth()
 			.sendPasswordResetEmail(email)
-			.then(function() {
+			.then(function () {
 				// Email sent.
 				setResetEmailSent(true);
 				setLoading(false);
 			})
-			.catch(function(error) {
+			.catch(function (error) {
 				// An error happened.
 				setLoading(false);
 			});
@@ -36,7 +36,7 @@ export default function Settings() {
 						<h1 className="text-center settings-h1">Account Settings</h1>
 					</Col>
 
-					<Col xs={12} className="p-3">
+					<Col md={8} className="p-3 mr-auto ml-auto">
 						<p className="settings-p m-0"><span className="font-weight-bold">Username:</span> {userData && userData.displayName}</p>
 						<p className="settings-p m-0"><span className="font-weight-bold">Group Code:</span> {userData && userData.code}</p>
 					</Col>
@@ -56,11 +56,24 @@ export default function Settings() {
 								passwordReset(currentUser.email);
 							}}
 						>
-							Reset password!
+							Reset Password
 						</Button>
 						{!resetEmailSent ? null : (
-							<Alert variant={'success'}>Your email has been sent. Please check your inbox!</Alert>
+							<Alert variant={"success"}>Your email has been sent. Please check your inbox!</Alert>
 						)}
+					</Col>
+				</div>
+			</Row>
+			<Row className="background-unset add-background-light pt-3 pb-4 border-top border-dark">
+				<div className="p-2 m-0 w-100">
+					<Col xs={12} className="mb-4">
+						<h2 className="text-center settings-h2">Want to support the app?</h2>
+					</Col>
+					<Col md={8} className="mr-auto ml-auto">
+						{/* <Button variant="dark background-dark w-100 mb-2">Report a Bug</Button> */}
+						<a href="https://www.patreon.com/dndnathan?fan_landing=true" target="blank">
+							<Button variant="dark background-dark w-100 mb-2">Visit our Patreon</Button>
+						</a>
 					</Col>
 				</div>
 			</Row>
