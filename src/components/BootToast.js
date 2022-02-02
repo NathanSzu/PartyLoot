6881eb55526b9393ed8ebd3640de3b62f67d585f
Nathan Szurek <nathan.szurek@yahoo.com>
2022-02-02 11:43:17 -0600

    fix: updated style for toast

--- src/components/BootToast.js
+++ src/components/BootToast.js
@@ -7,7 +7,7 @@ export default function BootToast() {
 
     return (
         <div className='position-fixed top-layer w-100 mt-3'>
-            <Toast className='ml-auto mr-auto' show={showToast} onClose={toggleShowToast} delay={15000} autohide>
+            <Toast className='ml-auto mr-auto background-light' show={showToast} onClose={toggleShowToast} delay={15000} autohide>
                 <Toast.Header className='justify-content-between'>
                     <strong className="me-auto">{toastHeader}</strong>
                 </Toast.Header>
