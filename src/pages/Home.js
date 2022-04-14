f04ceec041fd54cfd4af8357ec862d2b7e81976b
Nathan Szurek <nathan.szurek@yahoo.com>
2022-04-14 13:16:44 -0500

    fix: formatting update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,19 @@ import Login from '../components/BootLogin';
 import { Row, Col } from 'react-bootstrap';
 
 export default function Home() {
-	const [ login, setLogin ] = useState(null);
-	const [ welcome, setWelcome ] = useState(true);
+  const [login, setLogin] = useState(null);
+  const [welcome, setWelcome] = useState(true);
 
-	return (
-		<div className="p-0">
-			{welcome ? <Jumbotron setLogin={setLogin} setWelcome={setWelcome} /> : null}
-			<Row className="justify-content-md-center pl-3 pr-3">
-				<Col>{!welcome ? <Login login={login} setLogin={setLogin} /> : null}</Col>
-			</Row>
-		</div>
-	);
+  return (
+    <div className='p-0'>
+      {welcome ? (
+        <Jumbotron setLogin={setLogin} setWelcome={setWelcome} />
+      ) : null}
+      <Row className='justify-content-md-center pl-3 pr-3'>
+        <Col>
+          {!welcome ? <Login login={login} setLogin={setLogin} /> : null}
+        </Col>
+      </Row>
+    </div>
+  );
 }
