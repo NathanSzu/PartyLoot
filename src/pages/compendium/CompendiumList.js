0cb9bee5fcca71797886a3e837878d31d706cbcc
Nathan Szurek <nathan.szurek@yahoo.com>
2023-06-23 15:16:12 -0500

    feat: building out compendium ui

--- src/pages/compendium/CompendiumList.js
+++ src/pages/compendium/CompendiumList.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Col, Row } from 'react-bootstrap';
 import DetailsPanel from './DetailsPanel';
 
 export default function CompendiumList({ compendium }) {
   return (
-    <ListGroup>
+    <ListGroup className='pt-2'>
       {compendium?.map((item, idx) => (
-        <ListGroupItem key={item.id} id={`item${idx}`}>
-          {item.itemName}
-          <DetailsPanel />
+        <ListGroupItem className='rounded border-dark' key={item.id} id={`item${idx}`}>
+          <Row>
+            <Col>
+              <h1 className='item-h1 m-0'>{item.itemName}</h1>
+              <p className='m-0'>Likes | Category</p>
+            </Col>
+            <DetailsPanel item={item}/>
+          </Row>
         </ListGroupItem>
       ))}
     </ListGroup>
--- src/pages/compendium/DetailsPanel.js
+++ src/pages/compendium/DetailsPanel.js
@@ -1,26 +1,29 @@
 import React, { useState } from 'react';
-import { Button, Offcanvas } from 'react-bootstrap';
+import { Button, Col, Offcanvas } from 'react-bootstrap';
+import QuillDisplay from '../common/QuillDisplay';
 
-export default function DetailsPanel() {
+export default function DetailsPanel({item}) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const toggleShow = () => setShow((s) => !s);
 
   return (
-    <>
-      <Button variant='primary' onClick={toggleShow} className='me-2'>
-        Details
+    <Col xs={3} className='text-end'>
+      <Button className='h-100' variant='primary' onClick={toggleShow}>
+        View
       </Button>
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>{item?.itemName}</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists,
-          etc.
+        <QuillDisplay className='p-0' value={item?.itemDesc} />
+          <p>
+          Creator:
+          </p>
         </Offcanvas.Body>
       </Offcanvas>
-    </>
+    </Col>
   );
 }
