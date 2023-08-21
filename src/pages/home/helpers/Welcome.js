4aac4d2ca5c239349eb5d58e945e43fa30469878
Nathan Szurek <nathan.szurek@yahoo.com>
2023-08-21 15:32:51 -0500

    feat: updates to home screen pt 2

--- src/pages/home/helpers/Welcome.js
+++ src/pages/home/helpers/Welcome.js
@@ -10,7 +10,7 @@ export default function Welcome({ setWelcome, setLogin }) {
   return (
     <Container fluid className='background-light rounded-bottom p-3' data-cy='welcome-message'>
       <Row border='dark'>
-        <Col xs={12} className='mr-auto ml-auto'>
+        <Col xs={12} sm={10} md={8} className='ms-auto me-auto'>
           <div className='p-3 fs-5'>
             <h1 className='text-center mb-4 background-dark rounded text-light p-2'>Welcome to Party Loot</h1>
             <p>Track your adventuring inventory like never before!</p>
@@ -24,34 +24,25 @@ export default function Welcome({ setWelcome, setLogin }) {
               Tap "Get Started" to set up an account, or "Patreon" if you'd like to support this free application for
               others.
             </p>
+            <p className='mb-4'>
+              With this page open in your mobile browser remember to select <strong>add to home screen</strong> for the
+              full experience.
+            </p>
           </div>
         </Col>
       </Row>
-      <Row className='pb-3'>
-        <Col>
+      <Row className='pb-3 justify-content-center'>
+        <Col xs={4} className=''>
           <a href='https://www.patreon.com/dndnathan?fan_landing=true' target='blank'>
-            <Button variant='light' className='w-100 border-0'>
+            <Button variant='light' className='w-100 shadow'>
               Patreon
             </Button>
           </a>
         </Col>
-        <Col>
-          <Button variant='light' className='w-100 border-0' data-cy='get-started' onClick={showForms}>
+        <Col xs={4} className=''>
+          <Button variant='light' className='w-100 shadow' data-cy='get-started' onClick={showForms}>
             Get Started
           </Button>
-        </Col>
-      </Row>
-      <Row>
-        <Col xs={12} className='mr-auto ml-auto'>
-          <div className='p-3  fs-5'>
-            <h2 className='text-center mb-4 background-dark rounded text-light p-2'>
-              This is a Progressive Web Application
-            </h2>
-            <p className='mb-4'>
-              With this page open in your mobile browser select <strong>add to home screen</strong> for the full
-              experience.
-            </p>
-          </div>
         </Col>
       </Row>
     </Container>
